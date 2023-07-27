5710067a2a9945aad7ad09c7ff209677f1b867f8
Simen Norderud Jensen <simen.jensen@bouvet.no>
2023-07-27 16:35:47 +0200

    add some comments to TempObjectView.tsx

--- src/components/TempObjectView.tsx
+++ src/components/TempObjectView.tsx
@@ -1,5 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import SimpleTable from "./SimpleTable";
 
+/**
+ * Only meant as a temporary way to quickly display the data of objects returned from the backend on some pages for illustration purposes. Should be deleted not improved upon.
+ * @param props Any kind of object to display the data of
+ * @returns A ReactNode displaying the data of the object in an ugly way
+ */
 export function TempObjectView(props: { data: object }) {
   return (
     <SimpleTable
