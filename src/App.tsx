a5cdcae0d4bfd26bffebd74a6a5fb4a0aca46405
marasva <marie.a.svanes@gmail.com>
2023-08-11 10:48:46 +0200

    feat: add reservations as home

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <Home />,
+        element: <ReservationsOverview />,
       },
       {
         path: "locations",
